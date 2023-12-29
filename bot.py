46963eb5444e52519ff695bf8e07d436740a4ba1
sachinkpt <145140896+sachinkpt@users.noreply.github.com>
2023-12-29 13:55:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://graph.org/file/5ef5753c1200a0f29cf3e.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @illuminatiXNetwork__**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+            await m.reply_photo( caption="**Iam an Auto Join Request Approve Bot. Just Add me and Make Admin**", reply_markup=keyboard)
     
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
